e6e247d83a199f41132bcb958bbfbbedaa22793f
Misha <wrongsky1@yandex.ru>
2021-08-09 18:25:13 +0300

    add counters

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div className="">JavaScript - язык програмирования</div>
+        </div>
+        <div className="post__buttons">
+          <button>Удалить</button>
+        </div>
+      </div>
     </div>
   );
 }
