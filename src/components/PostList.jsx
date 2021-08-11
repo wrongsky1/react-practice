b12109538ea70717da127d3d93c9e60668f2fa09
Misha <wrongsky1@yandex.ru>
2021-08-11 11:27:52 +0300

    add animation for posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import PostItem from './PostItem'
+import {
+    CSSTransition,
+    TransitionGroup,
+  } from 'react-transition-group';
 
 function PostList({posts, title, remove}) {
     if (!posts.length) {
@@ -12,9 +16,18 @@ function PostList({posts, title, remove}) {
     return (
         <div>
             <h1 style={{textAlign:'center'}}>{title}</h1>
-            {posts.map((post, index) => 
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
-            )} 
+            <TransitionGroup>
+                {posts.map((post, index) => 
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )} 
+            </TransitionGroup>
+
         </div>
     )
 }
