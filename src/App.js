bd78ea2288e595ea171da086d5bc4442af4a99c5
Misha <wrongsky1@yandex.ru>
2021-08-13 12:55:32 +0300

    add pagination and usefetching

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React, {useState, useRef, useMemo, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
-import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton'
-import MyInput from './components/UI/input/MyInput';
 import MyModal from './components/UI/modal/MyModal';
-import MySelect from './components/UI/select/MySelect';
-import axios from 'axios';
 import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
+import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
 
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false)
 
+  const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
+    const response = await PostService.getAll(limit, page);
+    setPosts(response.data)
+    const totalCount = (response.headers['x-total-count'])
+    setTotalPages(getPageCount(totalCount, limit))
+  })
 
   useEffect(() => {
     fetchPosts()
-  }, [])
-
-  async function fetchPosts() {
-    setIsPostsLoading(true)
-    setTimeout( async () => {
-      const posts = await PostService.getAll();
-      setPosts(posts)
-      setIsPostsLoading(false)
-    }, 1000)
-  }
-
-
-
-
+  }, [page])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -48,28 +42,34 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  
+
+  const changePage = (page) => {
+    setPage(page)
+  }
 
   return (
     <div className="App">
-
-      
-
       <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
         Создать пост
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost}/>
       </MyModal>
-      
       <hr style={{margin: '15px 0'}}/>
       <PostFilter filter={filter} setFilter={setFilter}/>
+      {postError && 
+        <h1>Произошла ошибка ${postError}</h1>
+      }
       {isPostsLoading
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
           
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title={'Посты про JS'}/>
       }
-      
+      <Pagination 
+        page={page}
+        changePage={changePage} 
+        totalPage={totalPages}
+      />
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,14 +5,14 @@ function PostItem(props) {
     return (
         <div className="post">
             <div className="post__content">
-                <strong>{props.number} {props.post.title}</strong>
+                <strong>{props.post.id} {props.post.title}</strong>
                 <div className="">{props.post.body}</div>
             </div>
             <div className="post__buttons">
                 <MyButton onClick={() => props.remove(props.post)}>
                     Удалить
                 </MyButton>
-            </div>
+            </div> 
         </div>
     )
 }
