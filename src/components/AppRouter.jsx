5ce726941c6e6f5fce94adb0b1d93ef847154568
Misha <wrongsky1@yandex.ru>
2021-08-13 16:03:45 +0300

    add login and change routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,27 +1,41 @@
 import React from 'react'
-import { Redirect, Route, Switch} from 'react-router-dom'
+import { Redirect, Route, Router, Switch} from 'react-router-dom'
 import About from '../pages/About';
 import Posts from '../pages/Posts';
 import Error from '../pages/Error';
 import PostIdPage from '../pages/PostIdPage';
+import { privateRoutes, publicRoutes } from '../router';
 
 function AppRouter() {
+    const isAuth = true;
     return (
-        <Switch>
-            <Route path="/about">
-                <About/>
-            </Route>
-            <Route exact path="/posts">
-                <Posts/>
-            </Route>
-            <Route exact path="/posts/:id">
-                <PostIdPage/>
-            </Route>
-            <Route path="/error">
-                <Error/>
-            </Route>
-            <Redirect to="/error"/>
-        </Switch>
+        isAuth
+            ?
+            <Switch>
+                {privateRoutes.map(route => 
+                    <Route 
+                        component={route.component} 
+                        path={route.path} 
+                        exact={route.exact}/>
+                )}
+
+                <Redirect to="/posts"/>
+            </Switch>
+            :
+            <Switch>
+                {publicRoutes.map(route => 
+                    <Route 
+                        component={route.component} 
+                        path={route.path} 
+                        exact={route.exact}/>
+                )}
+
+                <Redirect to="/posts"/>
+            </Switch>
+
+
+
+
     )
 }
 
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import PostService from '../API/PostService'
+import Loader from '../components/UI/Loader/Loader'
+import { useFetching } from '../hooks/useFetching'
 
 function PostIdPage() {
     const params = useParams()
+    const [post, setPost] = useState({})
+    const [comments, setComments] = useState([])
+    const [fetchPostById, isLoading, error] = useFetching( async () => {
+        const response = await PostService.getById(params.id)
+        setPost(response.data)
+    })
+    const [fetchComments, isCommentsLoading, commentsError] = useFetching( async () => {
+        const response = await PostService.getCommentsByPostId(params.id)
+        setComments(response.data)
+    })
+
+    useEffect(() => {
+        fetchPostById(params.id)
+        fetchComments(params.id)
+    }, [])
+ 
+
     return (
         <div>
             <h2>Вы открыли страницу поста с ID = {params.id}</h2>
+            {isLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+                : <div>{post.id}. {post.title}</div>
+            }
+            <h3>Комментарии</h3>
+            {isCommentsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+                : <div>
+                    {comments.map(comment => 
+                        <div style={{marginTop: 15}}>
+                            <h5>{comment.email}</h5>
+                            <div>{comment.body}</div>
+                        </div>    
+                    )}
+                </div>
+            }
         </div>
     )
 }
